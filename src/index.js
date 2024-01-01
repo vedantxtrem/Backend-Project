24fb0a2526bc13256c381950136baffd219fa497
Vedant Sahu <vedant@pop-os.localdomain>
2024-01-01 14:14:03 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -7,6 +7,14 @@ dotenv.config({
     path:'./env'
 })
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 5000 ,()=>{
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGODB connection failed !!",err);
+})
 
 /*
 import express from "express";
